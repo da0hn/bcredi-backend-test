9f9daca13c6f962948cc3adfec377a8b70ff6b45
da0hn <gabrielhondacba@gmail.com>
2022-03-30 19:30:17 -0400

    refactor: change `timestamp` type to `LocalDateTime`

--- src/main/java/br/com/gabriel/analyzer/events/EventMetadata.java
+++ src/main/java/br/com/gabriel/analyzer/events/EventMetadata.java
@@ -1,11 +1,11 @@
 package br.com.gabriel.analyzer.events;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
 public record EventMetadata(
   String id,
   String schema,
   EventAction action,
-  ZonedDateTime timestamp
+  LocalDateTime timestamp
 ) {
 }
