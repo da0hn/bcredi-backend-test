c98a6a867e566961f242e34402267fd8c559e839
da0hn <gabrielhondacba@gmail.com>
2022-04-01 09:26:15 -0400

    refactor: move conversion of `schema` to enum `EventSchema`

--- src/main/java/br/com/gabriel/analyzer/EventSpliterator.java
+++ src/main/java/br/com/gabriel/analyzer/EventSpliterator.java
@@ -2,31 +2,27 @@ package br.com.gabriel.analyzer;
 
 import br.com.gabriel.analyzer.events.Event;
 import br.com.gabriel.analyzer.events.EventMetadataBuilder;
-import br.com.gabriel.analyzer.events.ProponentEventBuilder;
-import br.com.gabriel.analyzer.events.ProposalEventBuilder;
-import br.com.gabriel.analyzer.events.WarrantyEventBuilder;
+import br.com.gabriel.analyzer.events.EventSchema;
 
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.stream.BaseStream;
 import java.util.stream.Stream;
-
-import static br.com.gabriel.analyzer.events.EventAction.REMOVED;
 
 public class EventSpliterator implements Spliterator<Event> {
 
-  private static final long FIELD_NUMBER = 10;
   private final Spliterator<String> data;
 
 
-  public EventSpliterator(Stream<String> data) {
+  public EventSpliterator(final BaseStream<String, Stream<String>> data) {
     this.data = data.spliterator();
   }
 
-  @Override public boolean tryAdvance(Consumer<? super Event> action) {
+  @Override public boolean tryAdvance(final Consumer<? super Event> action) {
 
-    var eventBuilder = EventMetadataBuilder.anEventMetadata();
+    final var eventBuilder = EventMetadataBuilder.anEventMetadata();
 
-    var isEventParsed = SpliteratorActionApplier.apply(
+    final var isEventParsed = SpliteratorActionApplier.apply(
       this.data,
       eventBuilder::withId,
       eventBuilder::withSchema,
@@ -36,74 +32,9 @@ public class EventSpliterator implements Spliterator<Event> {
 
     if(!isEventParsed) return false;
 
-    return switch(eventBuilder.schema()) {
-      case PROPONENT -> {
-        var proponentBuilder = ProponentEventBuilder.aProponentEvent();
+    final EventSchema schema = eventBuilder.schema();
 
-        proponentBuilder.withEvent(eventBuilder.build());
-
-        var isProponentParsed = SpliteratorActionApplier.apply(
-          this.data,
-          proponentBuilder::withProposalId,
-          proponentBuilder::withId,
-          proponentBuilder::withName,
-          proponentBuilder::withAge,
-          proponentBuilder::withMonthlyIncome,
-          proponentBuilder::withIsMain
-        );
-
-        if(isProponentParsed) {
-          action.accept(proponentBuilder.build());
-          yield true;
-        }
-        yield false;
-      }
-      case PROPOSAL -> {
-        var proposalBuilder = ProposalEventBuilder.aProposalEvent();
-
-        proposalBuilder.withEvent(eventBuilder.build());
-
-        var isProposalParsed = SpliteratorActionApplier.apply(
-          this.data,
-          proposalBuilder::withId,
-          proposalBuilder::withLoanValue,
-          proposalBuilder::withNumberOfMonthlyInstallments
-        );
-
-        if(isProposalParsed) {
-          action.accept(proposalBuilder.build());
-          yield true;
-        }
-
-        yield false;
-      }
-      case WARRANTY -> {
-
-        var warrantyBuilder = WarrantyEventBuilder.aWarrantyEvent();
-        warrantyBuilder.withEvent(eventBuilder.build());
-
-        var isWarrantyParsed = SpliteratorActionApplier.apply(
-          this.data,
-          warrantyBuilder::withProposalId,
-          warrantyBuilder::withId
-        );
-
-        if(isWarrantyParsed && eventBuilder.action() != REMOVED) {
-          isWarrantyParsed = SpliteratorActionApplier.apply(
-            this.data,
-            warrantyBuilder::withValue,
-            warrantyBuilder::withWarrantyProvince
-          );
-        }
-
-        if(isWarrantyParsed) {
-          action.accept(warrantyBuilder.build());
-          yield true;
-        }
-
-        yield false;
-      }
-    };
+    return schema.convertFromSpliterator(this.data, eventBuilder.build(), action);
   }
 
   @Override public Spliterator<Event> trySplit() {
--- src/main/java/br/com/gabriel/analyzer/events/EventSchema.java
+++ src/main/java/br/com/gabriel/analyzer/events/EventSchema.java
@@ -1,17 +1,106 @@
 package br.com.gabriel.analyzer.events;
 
+import br.com.gabriel.analyzer.SpliteratorActionApplier;
+
 import java.util.Locale;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+import static br.com.gabriel.analyzer.events.EventAction.REMOVED;
 
 
 public enum EventSchema {
 
-  WARRANTY,
-  PROPONENT,
-  PROPOSAL;
+  WARRANTY {
+    @Override public boolean convertFromSpliterator(
+      final Spliterator<String> spliterator,
+      final EventMetadata event,
+      final Consumer<? super Event> action
+    ) {
+      final var warrantyBuilder = WarrantyEventBuilder.aWarrantyEvent();
+      warrantyBuilder.withEvent(event);
+
+      var isWarrantyParsed = SpliteratorActionApplier.apply(
+        spliterator,
+        warrantyBuilder::withProposalId,
+        warrantyBuilder::withId
+      );
+
+      if(isWarrantyParsed && event.action() != REMOVED) {
+        isWarrantyParsed = SpliteratorActionApplier.apply(
+          spliterator,
+          warrantyBuilder::withValue,
+          warrantyBuilder::withWarrantyProvince
+        );
+      }
+
+      if(!isWarrantyParsed) {
+        return false;
+      }
+      action.accept(warrantyBuilder.build());
+      return true;
+
+    }
+  },
+  PROPONENT {
+    @Override public boolean convertFromSpliterator(
+      final Spliterator<String> spliterator,
+      final EventMetadata event,
+      final Consumer<? super Event> action
+    ) {
+      final var proponentBuilder = ProponentEventBuilder.aProponentEvent();
+
+      proponentBuilder.withEvent(event);
+
+      final var isProponentParsed = SpliteratorActionApplier.apply(
+        spliterator,
+        proponentBuilder::withProposalId,
+        proponentBuilder::withId,
+        proponentBuilder::withName,
+        proponentBuilder::withAge,
+        proponentBuilder::withMonthlyIncome,
+        proponentBuilder::withIsMain
+      );
+
+      if(!isProponentParsed) {
+        return false;
+      }
+      action.accept(proponentBuilder.build());
+      return true;
+    }
+  },
+  PROPOSAL {
+    @Override public boolean convertFromSpliterator(
+      final Spliterator<String> spliterator,
+      final EventMetadata event,
+      final Consumer<? super Event> action
+    ) {
+      final var proposalBuilder = ProposalEventBuilder.aProposalEvent();
+
+      proposalBuilder.withEvent(event);
+
+      final var isProposalParsed = SpliteratorActionApplier.apply(
+        spliterator,
+        proposalBuilder::withId,
+        proposalBuilder::withLoanValue,
+        proposalBuilder::withNumberOfMonthlyInstallments
+      );
+
+      if(!isProposalParsed) return false;
+
+      action.accept(proposalBuilder.build());
+      return true;
+    }
+  };
 
   public String asRaw() {
     return this.name().toLowerCase(Locale.ROOT);
   }
 
+  public abstract boolean convertFromSpliterator(
+    Spliterator<String> spliterator,
+    EventMetadata event,
+    Consumer<? super Event> action
+  );
 
 }
